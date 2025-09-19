b62b5500bf45c9bb5e1f00c7483838119ec6f9ce
forkiepoo <inquires.fork@gmail.com>
2025-09-18 17:02:49 -0700

    Update faces.js

--- faces.js
+++ faces.js
@@ -1,9 +1,10 @@
-// faces.js
+// Define the cube faces
+// Use URLs for images OR "color:#hexcode" for solid colors
 const blockFaces = [
-  'https://upload.wikimedia.org/wikipedia/commons/7/70/User_icon_BLACK-01.png', // right
-  'https://i.imgur.com/4AiXzf8.png', // left
-  'color:#3498db', // top
-  'color:#2ecc71', // bottom
-  'https://i.imgur.com/Qr71crq.png', // front
-  'color:#ffffff' // back
+  "https://upload.wikimedia.org/wikipedia/commons/7/70/User_icon_BLACK-01.png", // right
+  "https://i.imgur.com/4AiXzf8.png", // left
+  "color:#3498db", // top
+  "color:#2ecc71", // bottom
+  "https://i.imgur.com/Qr71crq.png", // front
+  "color:#ffffff"  // back
 ];
