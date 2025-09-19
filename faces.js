321ea69398a88b03a29e19a8c6c843a6404759aa
forkiepoo <inquires.fork@gmail.com>
2025-09-18 17:01:54 -0700

    Create faces.js

---
+++ faces.js
@@ -0,0 +1,9 @@
+// faces.js
+const blockFaces = [
+  'https://upload.wikimedia.org/wikipedia/commons/7/70/User_icon_BLACK-01.png', // right
+  'https://i.imgur.com/4AiXzf8.png', // left
+  'color:#3498db', // top
+  'color:#2ecc71', // bottom
+  'https://i.imgur.com/Qr71crq.png', // front
+  'color:#ffffff' // back
+];
